839b5c9543227d09588318aab5bf696b2f7473e5
testgithubsonika <tomarsonika01@gmail.com>
2026-07-12 06:42:22 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,20 +1,26 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",   // or "mysql" depending on your DB
+});
+
 require('dotenv').config();
 
 //Force CLI to use the this config:
 //npx sequelize-cli db:seed:all --config config/config.js
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false, 
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false, 
+//   }
+// );
 
 const connectDB = async () => {
   try {
