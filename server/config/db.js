0ea8f4825deb5f1fd33a3359dc652e84d0915f3e
testgithubsonika <tomarsonika01@gmail.com>
2026-07-12 09:07:08 +0530

    ..

--- server/config/db.js
+++ server/config/db.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const { Sequelize } = require("sequelize");
-
+console.log("DATABASE_URL =", process.env.DATABASE_URL);
 // Initialize Sequelize once
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
@@ -11,6 +11,20 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     }
   }
 });
+//Force CLI to use the this config:
+//npx sequelize-cli db:seed:all --config config/config.js
+
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false, 
+//   }
+// );
 
 // Connection check helper
 const connectDB = async () => {
