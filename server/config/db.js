c579b3086871c114aed120e5d80600511dab4d90
testgithubsonika <tomarsonika01@gmail.com>
2026-07-13 03:24:43 +0530

    update

--- server/config/db.js
+++ server/config/db.js
@@ -3,29 +3,29 @@ console.log("DATABASE_URL =", process.env.DATABASE_URL);
 const { Sequelize } = require("sequelize");
 
 // // Initialize Sequelize once
-// const sequelize = new Sequelize(process.env.DATABASE_URL, {
-//   dialect: "postgres",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   }
-// });
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
 //Force CLI to use the this config:
 //npx sequelize-cli db:seed:all --config config/config.js
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    logging: false, 
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASSWORD,
+//   {
+//     host: process.env.DB_HOST,
+//     port: process.env.DB_PORT,
+//     dialect: process.env.DB_DIALECT,
+//     logging: false, 
+//   }
+// );
 
 // Connection check helper
 const connectDB = async () => {
