ec7ea6f99b1ef85d25580b95a05d47ac3e4aa4d5
testgithubsonika <tomarsonika01@gmail.com>
2026-07-12 16:44:29 +0530

    ..

--- server/config/config.js
+++ server/config/config.js
@@ -2,13 +2,17 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER || 'postgres',
-    password: process.env.DB_PASSWORD || null,
-    database: process.env.DB_NAME || 'your_dev_db',
-    host: process.env.DB_HOST || '127.0.0.1',
-    port: process.env.DB_PORT || 5432,
-    dialect: process.env.DB_DIALECT|| 'postgres',
-  },
+  use_env_variable: "DATABASE_URL",
+  dialect: "postgres",
+},
+  // development: {
+  //   username: process.env.DB_USER || 'postgres',
+  //   password: process.env.DB_PASSWORD || null,
+  //   database: process.env.DB_NAME || 'your_dev_db',
+  //   host: process.env.DB_HOST || '127.0.0.1',
+  //   port: process.env.DB_PORT || 5432,
+  //   dialect: process.env.DB_DIALECT|| 'postgres',
+  // },
   test: {
     username: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASSWORD || null,
@@ -18,11 +22,21 @@ module.exports = {
     dialect: process.env.DB_DIALECT || 'postgres',
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT || 'postgres', 
-  }
+  use_env_variable: "DATABASE_URL",
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+}
+  // production: {
+  //   username: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   database: process.env.DB_NAME,
+  //   host: process.env.DB_HOST,
+  //   port: process.env.DB_PORT,
+  //   dialect: process.env.DB_DIALECT || 'postgres', 
+  // }
 };
