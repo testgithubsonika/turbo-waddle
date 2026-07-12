4ab6a8eaaa29e7ce1894df918058f766bb704fe7
testgithubsonika <tomarsonika01@gmail.com>
2026-07-12 08:39:05 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const { Sequelize } = require("sequelize");
 // });
 
 require('dotenv').config();
-const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize");
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
