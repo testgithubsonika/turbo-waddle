4b05b1c93a639cbcba19e0f938167c48fb76e76b
testgithubsonika <tomarsonika01@gmail.com>
2026-07-12 06:59:23 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,23 @@
 // const { Sequelize } = require('sequelize');
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "postgres",   // or "mysql" depending on your DB
-});
+// const sequelize = new Sequelize(process.env.DATABASE_URL, {
+//   dialect: "postgres",   // or "mysql" depending on your DB
+// });
 
 require('dotenv').config();
+const { Sequelize } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+});
+module.exports = sequelize;
 
 //Force CLI to use the this config:
 //npx sequelize-cli db:seed:all --config config/config.js
